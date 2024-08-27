4d8b6dc5f6543cf3411bcabfb709f03d508e30fe
kubilaykaand <kubilaykaand@gmail.com>
2024-08-27 10:22:33 +0300

    arranged the key configs

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from typing import List
 from sqlalchemy import ForeignKey
 
 import hashlib
-
+import os
 
 '''
 Make sure the required packages are installed: 
@@ -34,7 +34,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
